9dc85ce26071645f65583b08ea2158e0d9601816
limitMe <code@myaiol.com>
2017-11-06 17:08:48 +1100

    Introduce Design

---
+++ SouthCrossArchitecture/Styles/Styles.swift
@@ -0,0 +1,23 @@
+//
+//  Styles.swift
+//  SouthCrossArchitecture
+//
+//  Created by Dian Zhong on 06/11/2017.
+//  Copyright © 2017 Dian Zhong. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+typealias S = Styles
+
+/**
+ Styles perform exactly same as Constraints, but only specifically on Design and Style
+ */
+class Styles{
+    static let exampleStyleConstraint: CGFloat = 16
+    
+    struct ExampleStyleGroup {
+        static let exampleIndirectStyle: CGFloat = 32
+    }
+}
