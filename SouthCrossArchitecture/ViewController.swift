352bbaf0bba624bc1e71327cefe8dfcd929f46d3
limitMe <code@myaiol.com>
2017-11-06 16:49:07 +1100

    Initial File Structure

---
+++ SouthCrossArchitecture/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SouthCrossArchitecture
+//
+//  Created by Dian Zhong on 06/11/2017.
+//  Copyright © 2017 Dian Zhong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
